25fc672dd7c7e997a6ec731a2aa093d5cee323ac
Fortino Samuel <67204076+samuel-velazquez@users.noreply.github.com>
2020-10-09 23:05:06 -0600

    Add files via upload
    Plugin as set and accounter list or dynamic getso have stage is rel so component here

---
+++ Feature.ps1
@@ -0,0 +1,80 @@
+<#endregion
+.label
+.form
+.header
+.rel=macket
+.soap
+.mp34
+.is Describe "DescribeName" {
+    TM_SELECTED_TEXT
+}
+#>
+{
+    Open-EditorFile: .aspx .\mail.txt [PSCustomObject]@{
+        Name = Value
+Backup-AzureRmBackupItem ´><><<´ .\License: [<switch ($x)
+{
+    'value1' {  }
+    {$_ -in 'A','B','C'} {}
+    'value3' {}
+    Default {}
+}>]    }
+}[
+    type=function Get-TargetResource {
+        # TODO: Add parameters here
+        # Make sure to use the same parameters for
+        # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+        param (
+        )
+    }
+    function Set-TargetResource {
+        # TODO: Add parameters here
+        # Make sure to use the same parameters for
+        # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+        param (
+        )
+    }
+    function Test-TargetResource {
+        # TODO: Add parameters here
+        # Make sure to use the same parameters for
+        # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+        param (
+     LicenseManagerShellext.exe   )
+    }
+]
+{
+    propuse left TM_SELECTED_TEXT _main_name ().\BlobTrigger1
+    function FunctionName (OptionalParameters) {
+        
+ $env:VSCODE_GIT_IPC_HANDLE and .\README.md $Var = @{
+     Name = Value
+ Describe "DescribeName" {
+     Context "ContextName" {
+         It "ItName" {
+             Assertion
+         }
+     }
+ }}(format.com is .\host.json (.json configuration Name {
+     # One can evaluate expressions to get the node list
+     # E.g: $AllNodes.Where("Role -eq Web").NodeName
+     node ("Node1","Node2","Node3")
+     {
+         # Call Resource Provider
+         # E.g: WindowsFeature, File
+         WindowsFeature FriendlyName
+         {
+             Ensure = "Present"
+             Name = "Feature Name"
+         }
+ 
+         File FriendlyName
+         {
+             Ensure = "Present"
+             SourcePath = $SourcePath
+             DestinationPath = $DestinationPath
+             Type = "Directory"
+             DependsOn = "[WindowsFeature]FriendlyName"
+         }
+     }
+ }))   }
+}
